03e44c9684d68d55f45c6fa70029e73099ab1237
kimdaye0000 <daye0529@cu.ac.kr>
2022-04-16 00:24:57 +0900

    fibo

--- fibo.py
+++ fibo.py
@@ -3,17 +3,18 @@
 num = [0, 1, 0]
 i = 0
 n = 2
-i = int(input("fibonacci number입력 : "))
+i = int(input("fibonacci number: "))
 
 if i >= 2:
-    print(f"{num[0]} + 0 = {num[2]}")
+    print(f"{num[1]}") 
     while n <= i:
        num[2] = num[1] + num[0]
-       print(f"{num[0]} + {num[1]} = {num[2]}")
        num[0] = num[1]
        num[1] = num[2]
        n += 1
+       print(f"{num[1]}")	
 else:
     print("Error : 2 이상의 자연수를 입력해주세요.")
+print(f"F{i} = {num[2]}")
 
-print(f"\n{i}번째 피보나치 수열의 값은 {num[2]}입니다.")
+
