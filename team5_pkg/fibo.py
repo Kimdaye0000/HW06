09f360582fe529b352a1bfef2f00a48d267eb8df
nanocode00 <rwg0901@naver.com>
2022-04-16 16:41:26 +0900

    moved fibo.py

---
+++ team5_pkg/fibo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+num = [0, 1, 0]
+i = int(input("fibonacci number : "))
+
+if i >= 1:
+    while n <= i:
+       print(f"{num[1]}",end=' ')
+       num[2] = num[1] + num[0]
+       num[0] = num[1]
+       num[1] = num[2]
+       n += 1
+    print(f"{num[1]}")
+    print(f"F{i} = {num[1]}")
+else:
+    print("Error : 1 이상의 자연수를 입력해주세요.")
