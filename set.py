adc41fb5f8c9515efa4a57a421d9b5ca04f013e6
byungwoo0601 <rlaquddn0601@naver.com>
2022-04-16 14:18:44 +0900

    set

---
+++ set.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+
+print("input the 1st list : ",end='')
+list1=list(input().split())
+
+print("input the 2st list : ",end='')
+liar2=list(input().split())
+
+print(list1)
+print(list2)
